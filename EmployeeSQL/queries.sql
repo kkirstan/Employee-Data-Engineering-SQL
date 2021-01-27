1554e40caa81e38b8d3ac96700e825f95dc06a69
Kirstan Komajda <73805126+kkirstan@users.noreply.github.com>
2021-01-26 20:58:40 -0500

    finished code

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -19,3 +19,46 @@ ON departments.dept_no=dept_managers.dept_no
 JOIN employees
 ON employees.emp_no=dept_managers.emp_no;
 
+SELECT dept_employees.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name
+FROM dept_employees
+JOIN departments
+ON departments.dept_no=dept_employees.dept_no
+JOIN employees
+ON employees.emp_no=dept_employees.emp_no;
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' 
+AND last_name 
+LIKE 'B%';
+
+SELECT employees.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name
+FROM employees
+INNER JOIN dept_employees
+ON employees.emp_no=dept_employees.emp_no
+INNER JOIN departments
+ON departments.dept_no=dept_employees.dept_no
+WHERE departments.dept_name='Sales';
+
+SELECT employees.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name
+FROM employees
+INNER JOIN dept_employees
+ON employees.emp_no=dept_employees.emp_no
+INNER JOIN departments
+ON departments.dept_no=dept_employees.dept_no
+WHERE departments.dept_name='Sales'
+OR departments.dept_name='Development';
+
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
