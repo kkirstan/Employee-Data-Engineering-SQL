561f7cfffa67fec4809155b94fd7795e12c51bca
Kirstan Komajda <73805126+kkirstan@users.noreply.github.com>
2021-01-30 15:27:17 -0500

    reordered tables

--- EmployeeSQL/sql-tables.sql
+++ EmployeeSQL/sql-tables.sql
@@ -1,6 +1,28 @@
+CREATE TABLE employees(
+emp_no INT PRIMARY KEY,
+emp_title_id VARCHAR,
+birth_date VARCHAR,
+first_name VARCHAR,
+last_name VARCHAR,
+sex VARCHAR,
+hire_date VARCHAR
+);
+
+
 CREATE TABLE departments(
 dept_no VARCHAR PRIMARY KEY,
 dept_name VARCHAR
+);
+
+CREATE TABLE titles(
+title_id VARCHAR PRIMARY KEY,
+title VARCHAR
+);
+
+CREATE TABLE salaries(
+emp_no INT PRIMARY KEY,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+salary INT
 );
 
 CREATE TABLE dept_employees(
@@ -17,26 +39,4 @@ FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 emp_no INT,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 PRIMARY KEY (dept_no, emp_no)
-);
-
-CREATE TABLE employees(
-emp_no INT PRIMARY KEY,
-emp_title_id VARCHAR,
-FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
-birth_date VARCHAR,
-first_name VARCHAR,
-last_name VARCHAR,
-sex VARCHAR,
-hire_date VARCHAR
-);
-
-CREATE TABLE salaries(
-emp_no INT PRIMARY KEY,
-FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-salary INT
-);
-
-CREATE TABLE titles(
-title_id VARCHAR PRIMARY KEY,
-title VARCHAR
 );
